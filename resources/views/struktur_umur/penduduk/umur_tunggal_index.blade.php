2e888f1673cd3085960dfc09ff8c1623365ebf3c
arsyar21 <ariesyarwani21@gmail.com>
2025-01-30 11:27:01 +0800

    feat - layout desain penduduk

---
+++ resources/views/struktur_umur/penduduk/umur_tunggal_index.blade.php
@@ -0,0 +1,76 @@
+@extends('layout.master')
+@section('content')
+    <div class="container-fluid">
+        <div class="row page-titles">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">Penduduk</a></li>
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Umur Tunggal</a></li>
+            </ol>
+        </div>
+        <div class="col-xl-12 col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Semester & Tahun</h4>
+                </div>
+                <div class="card-body">
+                    <div class="basic-form">
+                        <form>
+                            <div class="row">
+                                <div class="col-md-4 mb-3">
+                                    <select name="semester" id="semester" class="form-control default-select">
+                                        <option value="" disabled selected>--PILIH Semester--</option>
+                                        <option value="1">I</option>
+                                        <option value="2">II</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-4 mb-3">
+                                    <select name="tahun" id="tahun" class="form-control default-select">
+                                    </select>
+                                </div>
+                                <div class="col-md-4 mb-3">
+                                    <select name="umur" id="umur">
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-list"></i> Tampilkan
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Penduduk - Umur Tunggal <span id="tahunSemester">I 2025</span></h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="table" class="display" style="min-width: 845px">
+                                <thead>
+                                    <tr>
+                                        <th>Kecamatan</th>
+                                        <th>Kelurahan</th>
+                                        <th>Umur</th>
+                                        <th>LK</th>
+                                        <th>PR</th>
+                                        <th>JUMLAH</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('elements.data_table')
+    <script>
+        $("#umur").select2();
+        selectAge()
+    </script>
+@endsection
