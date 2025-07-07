5bdc85b62db9d6281f36ff9693b370d4cc15211d
arsyar21 <ariesyarwani21@gmail.com>
2025-07-07 14:16:05 +0800

    fix - name class

--- app/Imports/StrukturUmurKepalaKeluargaStatusKawinImport.php
+++ app/Imports/StrukturUmurKepalaKeluargaStatusKawinImport.php
@@ -7,7 +7,7 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Str;
 
-class StrukturUmurKepalaKeluargaStatusKawinKelompokUmurImport implements ToModel, WithHeadingRow
+class StrukturUmurKepalaKeluargaStatusKawinImport implements ToModel, WithHeadingRow
 {
     protected $tahun;
     protected $semester;
