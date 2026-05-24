9972bd37270cf6be3f664ec4e7ed83463717c330
Gibran699 <gibran.ivantry@student.smkti.net>
2026-05-24 12:55:56 +0800

    fix: explicitly load metisMenu.min.js before custom.js

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -257,6 +257,7 @@
         Scripts
     ***********************************-->
     <script src="{{ asset('libs/global/global.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('libs/metismenu/js/metisMenu.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('libs/bootstrap-select/dist/js/bootstrap-select.min.js') }}" type="text/javascript"></script>
 
     {{-- <script src="{{ asset('js/plugins-init/select2-init.js') }}" type="text/javascript"></script> --}}
